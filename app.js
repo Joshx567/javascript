4dc6b8f3d767a877873f63379d57a7349d13465f
Jose <jose.lopez.s@ucb.edu.bo>
2023-04-20 17:59:44 -0400

    cambios

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-let x = 10;
-let y = 20;
-let suma = x + y;
-console.log(suma);
+//let x = 10;
+//let y = 20;
+//let suma = x + y;
+//console.log(suma);
+
+// ESTRUCTURA LEXICA
+// ----------------------- case-sensitive
+// let A = 10;
+// if(A>5)
+// {
+//     console.log("Es mayor a 5")
+// }
+// ------------------------ comentarios
+// Comentario de una linea
+/*
+    linea 1
+    linea 2
+*/
+// ------------------------------------- literal
+// 10;
+// 'HOLA MUNDO';
+// true;
+// false;
+// ----------------------------------------- identificadores
+// let nombre = 'Juan'; //Valido
+// let $precio = 100.5; //Valido
+// //let 5var = 5; //no valido
+// let _id = 'c';  //valido
+// --------------------------------------------- palabras reservadas 
+// break
+// case 
+// ......   
